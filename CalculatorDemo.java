507778c8959be3fce052a77fa19d9a2baee99a5b
Mike <khan1han@users.noreply.github.com>
2017-07-31 21:30:44 -0700

    java

---
+++ CalculatorDemo.java
@@ -0,0 +1,11 @@
+package org.javatutorial.javatutorials.object;
+
+public class CalculatorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(10 + 20);
+		System.out.println(20 + 40);
+	}
+
+}
